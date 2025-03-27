1eac806c0c56745cbb64bf3431cb67dbc35441e8
defenfiver <defenfiver45@gmail.com>
2025-03-27 10:49:58 -0400

    commiting ideas

--- front-end.js
+++ front-end.js
@@ -1,6 +1,19 @@
 const url = 'https://gutendex.com/books?search=';
 const bookurl = 'https://gutendex.com/books/'
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+async function askQuestion(prompt) {
+    return new Promise(resolve => {
+        rl.question(prompt, answer => {
+            resolve(answer);
+        })
+    });
+}
+
 async function getData(str) {
     const request = await fetch(url + str);
     const json = await request.json();
@@ -10,7 +23,12 @@ async function getData(str) {
     //have user pick the which id they want
     const newRequest = await fetch(bookurl + 221); //Then add
     // parse for "text/plain: charset=us-ascii"
+    return book
     
 }
 
-getData("sherlock");
+async function mainLoop(){
+    const answer = await askQuestion("What is your search query?");
+    book = getData(answer);
+    // parse and print book
+}
