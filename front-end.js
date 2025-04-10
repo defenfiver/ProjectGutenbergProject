4603dce8f66b0df2732bc36db0a8970f20524ed3
defenfiver <defenfiver45@gmail.com>
2025-04-10 12:45:58 -0400

    It works.

--- front-end.js
+++ front-end.js
@@ -1,5 +1,6 @@
 const url = 'https://gutendex.com/books?search=';
 const bookurl = 'https://gutendex.com/books/'
+const readline = require('readline')
 
 //From in class example
 const rl = readline.createInterface({
@@ -20,25 +21,26 @@ async function cacheControl(book, cache){
     //check if 10 books
     //if 10, add delete oldest
     // add new {book}
-    return cache
+    return cache;
 }
 
 async function getData(str) {
     const request = await fetch(url + str);
     const json = await request.json();
     for (let i = 0; i < json.count; ++i) {
-        console.log(json.result[i].id + json.results[i].title);
+        console.log(json.results[i].id + ": " +  json.results[i].title);
     }
     const id = await askQuestion("Which id book would you like to read?");
     const newRequest = await fetch(bookurl + id); 
-    parsedurl = search newRequest for //"text/plain: charset=us-ascii"
+    const requestJson = await newRequest.json();
+    parsedurl = requestJson["formats"]["text/plain; charset=us-ascii"]
     const book = await fetch(parsedurl);
-    return book
-    
+    const utf = await book.text();
+    return utf
 }
 
 async function printBook(book){
-    console.log(book)
+    console.log(book);
 }
 
 async function mainLoop(){
@@ -52,6 +54,8 @@ async function mainLoop(){
         book = getData(answer);
         // parse and print book
         cache = await cacheControl(book, cache);
+        printBook(book);
     }
-    
 }
+
+getData("sherlock");
