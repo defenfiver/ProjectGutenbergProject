8238866d30568fda4f3763a664493972d0763dcf
defenfiver <defenfiver45@gmail.com>
2025-04-13 23:06:09 -0400

    Added some error handling

--- front-end.js
+++ front-end.js
@@ -27,26 +27,44 @@ async function cacheControl(book, cache){
 async function getData(str) {
     const request = await fetch(url + str);
     const json = await request.json();
+    let idArray = [];
     for (let i = 0; i < json.count; ++i) {
         try {
             console.log(json.results[i].id + ": " + json.results[i].title);
+            idArray.push(json.results[i].id)
         } catch (TypeError) {}
-
     }
-    const id = await askQuestion("Which id book would you like to read?");
-    const newRequest = await fetch(bookurl + id); 
-    const requestJson = await newRequest.json();
-    parsedurl = requestJson["formats"]["text/plain; charset=us-ascii"]
-    const book = await fetch(parsedurl);
-    const utf = await book.text();
-    return utf
+    if (idArray.length == 0) {
+        console.log("No results found");
+        return 0;
+    } else {
+        while (1 == 1){
+            const id = await askQuestion("Which id book would you like to read?\n");
+            for (let i = 0; i < idArray.length; ++i) {
+                if (idArray[i] == id) {
+                    const newRequest = await fetch(bookurl + id);
+                    const requestJson = await newRequest.json();
+                    try {
+                        parsedurl = requestJson["formats"]["text/plain; charset=us-ascii"]
+                    } catch (TypeError) {
+                        console.log("No valid format was found for that text");
+                        return 0;
+                    }
+                    const book = await fetch(parsedurl);
+                    const utf = await book.text();
+                    return utf
+                }
+            }
+            console.log("That id is not apart of the results listed")
+        }
+    }
 }
 
 async function printBook(book){
     book = book.split(" ")
     let page_num = 1;
     let page = "";
-    console.log("Press enter to get to the next page")
+    console.log("\n\n\n\n\n\n\n\n\nPress enter to get to the next page")
     for (let i = 0; i < book.length; i++) {
         // if 250 words, then new page starts
         if (i % 250 == 0){
@@ -70,7 +88,7 @@ async function printBook(book){
 }
 
 async function mainLoop(){
-    const answer = await askQuestion("What is your search query? or enter .recent for your recent books");
+    const answer = await askQuestion("What is your search query? or enter .recent for your recent books\n");
     cache = [];
     if (answer == ".recent"){
         console.log(cache);
@@ -79,7 +97,14 @@ async function mainLoop(){
     } else{
         book = await getData(answer);
         //cache = await cacheControl(book, cache);
-        printBook(book);
+        if (book == 0) {
+            mainLoop();
+            return
+        } else if (book == 1) {
+            return;
+        } else {
+            printBook(book);
+        }
     }
 }
 
