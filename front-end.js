0af281ead688c11695b22f9a8b7307923435ecf6
defenfiver <defenfiver45@gmail.com>
2025-04-14 01:11:35 -0400

    Merged with error handling

--- front-end.js
+++ front-end.js
@@ -50,30 +50,40 @@ async function getData(str) {
         try {
             console.log(json.results[i].id + ": " + json.results[i].title);
             idArray.push(json.results[i].id)
-        } catch (TypeError) {}
+        } catch (TypeError) { }
     }
-
-    const id = await askQuestion("Which id book would you like to read?");
-    const newRequest = await fetch(bookurl + id); 
-    const requestJson = await newRequest.json();
-    parsedurl = requestJson["formats"]["text/plain; charset=us-ascii"]
-    console.log(parsedurl);
-    const title = requestJson.title;
-
-    const book = await fetch(parsedurl);
-    console.log(book)
-    const utf = await book.text();
-    return [utf, title]
-
+    if (idArray.length == 0) {
+        console.log("No results found");
+        return 0;
+    } else {
+        while (1 == 1) {
+            const id = await askQuestion("Which id book would you like to read?\n");
+            for (let i = 0; i < idArray.length; ++i) {
+                if (idArray[i] == id) {
+                    const newRequest = await fetch(bookurl + id);
+                    const requestJson = await newRequest.json();
+                    try {
+                        parsedurl = requestJson["formats"]["text/plain; charset=us-ascii"]
+                    } catch (TypeError) {
+                        console.log("No valid format was found for that text");
+                        return 0;
+                    }
+                    const title = requestJson.title;
+                    const book = await fetch(parsedurl);
+                    const utf = await book.text();
+                    return [utf, title]
+                }
+            }
+            console.log("That id is not apart of the results listed")
+        }
+    }
 }
-
-   
 
 async function printBook(book){
     book = book.split(" ")
     let page_num = 1;
     let page = "";
-    console.log("\n\n\n\n\n\n\n\n\nPress enter to get to the next page")
+    console.log("\n\n\n\n\n\n\n\n\nPress enter to get to the next page or stop to stop reading")
     for (let i = 0; i < book.length; i++) {
         // if 250 words, then new page starts
         if (i % 250 == 0){
@@ -81,6 +91,10 @@ async function printBook(book){
             page_num += 1;
             console.log(page);
             x = await askQuestion("")
+            if (x == "stop") {
+                console.log("Stopping Reading")
+                return
+            }
             page = "";
         }
         // This checks to see if it is the first word on the page and adds the word to the variable  
@@ -94,14 +108,21 @@ async function printBook(book){
             page += word
         }
     }
+    return
+}
+
+async function printCache(cache) {
+    for (let i = 0; i < cache.length; ++i) {
+        console.log("\t" + i + ": " + cache[i].key);
+    }
 }
 
 async function mainLoop(){
     while (true) {
     // cache = [];
-        const answer = await askQuestion("What is your search query? or enter .recent for your recent books");
+        const answer = await askQuestion("What is your search query? or enter .recent for your recent books\n");
         if (answer == ".recent"){
-            console.log(cache);
+            printCache(cache);
             const answer = await askQuestion("What order in the queue (numbers: 0-9) is the book that you want");
             const num = parseInt(answer)
             // printBook(cache[answer])
@@ -109,9 +130,12 @@ async function mainLoop(){
             // console.log(cache[num].value)
         } else{
             the_answer = await getData(answer)
-            title = the_answer[1];
-            book = the_answer[0];
-            cache = await cacheControl(book, cache, title);
+            if (the_answer != 0) {
+                title = the_answer[1];
+                book = the_answer[0];
+                cache = await cacheControl(book, cache, title);
+                none = await printBook(book);
+            }
         }
     }
 }
